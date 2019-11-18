09bfb832cf3e66e9c3d24b2d18c22b86b021b305
Jugal <jugaldeepak@gmail.com>
2019-11-19 02:31:36 +0530

    page not found;

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { TodoComponent } from './todo/todo.component';
 import { AuthGuard } from './auth.guard';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'todo', component: TodoComponent, canActivate: [AuthGuard] }
+  { path: 'todo', component: TodoComponent, canActivate: [AuthGuard] },
+  { path: '**', component: PagenotfoundComponent }
 ];
 
 @NgModule({
