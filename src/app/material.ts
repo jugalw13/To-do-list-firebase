f980b8d34db9b9c02841f54741677eeb15270185
Jugal <jugaldeepak@gmail.com>
2019-11-19 12:13:31 +0530

    UI for to do

--- src/app/material.ts
+++ src/app/material.ts
@@ -5,10 +5,11 @@ import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
-  imports: [MatInputModule, MatFormFieldModule, MatIconModule, MatCardModule, MatButtonModule, MatToolbarModule],
-  exports: [MatInputModule, MatFormFieldModule, MatIconModule, MatCardModule, MatButtonModule, MatToolbarModule],
+  imports: [MatInputModule, MatFormFieldModule, MatIconModule, MatCardModule, MatButtonModule, MatToolbarModule, MatCheckboxModule],
+  exports: [MatInputModule, MatFormFieldModule, MatIconModule, MatCardModule, MatButtonModule, MatToolbarModule, MatCheckboxModule],
 })
 
 export class MatModule { }
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -8,7 +8,30 @@ import { NavbarService } from '../navbar.service';
 })
 export class TodoComponent implements OnInit {
 
+  inputItem: string;
+  todoList = [];
+  count = 0;
+
   constructor(private navbarService: NavbarService) { }
+
+  addItem() {
+    const item = {
+      complete: false,
+      name: this.inputItem
+    };
+    if (this.todoList.filter(test => test.name === item.name).length === 0) {
+      this.todoList.push(item);
+    }
+    this.inputItem = '';
+  }
+
+  deleteItem(item: any) {
+    this.todoList = this.todoList.filter(todo => todo.name !== item.name);
+  }
+
+  editItem(item) {
+    item.complete = !item.complete;
+  }
 
   ngOnInit() {
     this.navbarService.show();
