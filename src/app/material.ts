c8e0e035912b23bc3d58094d1d30603c58c79a00
Jugal <jugaldeepak@gmail.com>
2019-11-19 12:03:15 +0530

    final navbar

--- src/app/material.ts
+++ src/app/material.ts
@@ -4,10 +4,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  imports: [MatInputModule, MatFormFieldModule, MatIconModule, MatCardModule, MatButtonModule],
-  exports: [MatInputModule, MatFormFieldModule, MatIconModule, MatCardModule, MatButtonModule]
+  imports: [MatInputModule, MatFormFieldModule, MatIconModule, MatCardModule, MatButtonModule, MatToolbarModule],
+  exports: [MatInputModule, MatFormFieldModule, MatIconModule, MatCardModule, MatButtonModule, MatToolbarModule],
 })
 
 export class MatModule { }
